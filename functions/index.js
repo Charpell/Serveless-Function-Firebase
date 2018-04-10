542c980449a2136e4e78480ba5be362d123af2ed
Charpell <charpellumeh@gmail.com>
2018-04-10 11:45:01 +0100

    Generate Service account for admins

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
+const admin = requre('firebase-admin')
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-e5d4e.firebaseio.com"
 });
+
+exports.createUser = functions.https.onRequest(createUser)
