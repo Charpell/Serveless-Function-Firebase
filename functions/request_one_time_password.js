f1f7c122e55905de883c8f1c858290906e281b62
Charpell <charpellumeh@gmail.com>
2018-04-10 16:17:12 +0100

    Sending text to users

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const twilio = require('./twilio')
 
 
 module.exports = function(req, res) {
@@ -10,6 +11,15 @@ module.exports = function(req, res) {
 
   admin.auth().getUser(phone)
     .then(userRecord => {
-      
+      const code = Math.floor((Math.random() * 8999 + 1000));
+
+      twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: phone,
+        from: '+13522681352'
+      })
+    })
+    .catch((error) => {
+      res.status(422).send({ error })
     })
 }
