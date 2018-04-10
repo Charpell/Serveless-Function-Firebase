d1795b6a79fa8dd084d0ff0cc1d408c3723770a2
Charpell <charpellumeh@gmail.com>
2018-04-10 12:34:36 +0100

    Successfully create new user

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,4 +1,4 @@
-const admon = require('firebase-admin');
+const admin = require('firebase-admin');
 
 module.exports = function(req, res) {
   // Verify the user provided a phone
@@ -10,6 +10,10 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   // Create a new user account using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }))
 
   // Respond to the user request, saying the account was made
+
 }
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const admin = requre('firebase-admin')
+const admin = require('firebase-admin')
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json')
